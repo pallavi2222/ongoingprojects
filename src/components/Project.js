dbefd5dadfa459122073d309cc82e28c0016818c
pallavi2222 <86560977+pallavi2222@users.noreply.github.com>
2021-07-07 10:06:53 +0530

    commitone

---
+++ src/components/Project.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Logo from '../images/google.jpg'
+import {Row,Column,Box,Images,Headline,Contents,Totalheadine,Arrow} from "../styles/project.style";
+import ArrowRightAltIcon from '@material-ui/icons/ArrowRightAlt';
+function Project() {
+    return (
+        <div>
+            <Row>
+                <Column>
+                <Box >
+                    
+                    <Images src="https://static.wixstatic.com/media/nsplsh_79714c727149736c323934~mv2.jpg/v1/fill/w_326,h_245,al_c,q_80,usm_0.66_1.00_0.01/Image%20by%20Praveen%20kumar%20Mathivanan.webp"/>
+                <Totalheadine>
+                <Headline>Digital Literacy & Cyber 
+                    Security</Headline>
+                    <Arrow>
+                        <ArrowRightAltIcon style={{color:"#d55342",fontSize:"60"}}></ArrowRightAltIcon>
+                    </Arrow>
+                    </Totalheadine>
+                <Contents>DLCS will bring awareness in the society to use digital technology efficiently and help them reduce their functional disability using it to their comfort. We also teach them the threat and health of Internet. How to be safe on them and the behaviour using it.</Contents>
+                </Box>
+                   
+                </Column>
+                <Column>
+                <Box></Box>
+                </Column>
+                <Column>
+                <Box></Box>
+                </Column>
+            </Row>
+        </div>
+    )
+}
+
+export default Project
---
+++ src/styles/project.style.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+export const Row = styled.div`
+margin-top:10px;
+height:426px;
+width: 100%;
+display: flex;
+`;
+export const Column = styled.div`
+width: 384.5px;
+height: 426px;
+`;
+export const Box = styled.div`
+margin-right:50px;
+margin-left: 40px;
+width:300px;
+height:426px;
+background-color: rgb(10, 19, 60);
+border-radius:7px;
+
+`;
+export const Images = styled.img`
+width:300px;
+height:175px;
+border-radius:7px;
+`;
+export const Contents = styled.p`
+color:white;
+font-size: 13.5px;
+width:276px;
+font-family: helvetica-w01-roman,helvetica-w02-roman,helvetica-lt-w10-roman,sans-serif;
+line-height:20px;
+padding:0px 13px;
+text-align: left;
+`;
+export const Totalheadine = styled.div`
+width:299px;
+height:70px;
+display: flex;
+`;
+export const Headline = styled.p`
+color:white;
+font-size:15px;
+flex: 111%;
+font-family: helvetica-w01-bold,helvetica-w02-bold,helvetica-lt-w10-bold,sans-serif;
+font-weight: bold;
+line-height: 20px;
+margin-left: 16px;
+text-align: left;
+`;
+export const Arrow = styled.div`
+flex:50%;
+color:white;
+`;
