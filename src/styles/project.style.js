199f68811c5b2f26e20862a2eb8f0026fb61cbf0
pallavi2222 <86560977+pallavi2222@users.noreply.github.com>
2021-07-08 07:41:22 +0530

    commit4

--- src/styles/project.style.js
+++ src/styles/project.style.js
@@ -5,8 +5,8 @@ height:426px;
 width: 100%;
 display: flex;
 @media only screen and (max-width: 500px) {
-    height:1700px ;
-     width:384.5px;
+    height:2000px;
+     width:400px;
      display:inherit;
     
     }
@@ -15,6 +15,7 @@ export const Column = styled.div`
 width: 384.5px;
 height: 426px;
 
+
 `;
 export const Box = styled.div`
 margin-right:50px;
@@ -22,10 +23,9 @@ margin-left: 40px;
 @media only screen and (max-width: 500px) {
     margin-right:10px;
  margin-left: 10px;
- margin-top:60px;
- width:375;
- height:730;
-    
+ margin-top:153px;
+ width:400px;
+  height:524px ;
     }
 width:300px;
 height:426px;
@@ -37,6 +37,10 @@ export const Images = styled.img`
 width:300px;
 height:175px;
 border-radius:7px;
+@media only screen and (max-width: 500px) {
+ width:400px;
+  height:210px ;
+    }
 `;
 export const Contents = styled.p`
 color:white;
@@ -46,11 +50,20 @@ font-family: helvetica-w01-roman,helvetica-w02-roman,helvetica-lt-w10-roman,sans
 line-height:20px;
 padding:0px 13px;
 text-align: left;
+@media only screen and (max-width: 500px) {
+    width:310px;
+    font-size: 14px;
+    line-height:21px;
+    }
 `;
 export const Totalheadine = styled.div`
 width:299px;
 height:70px;
 display: flex;
+@media only screen and (max-width: 500px) {
+    width:300px;
+    height: 100px;
+    }
 `;
 export const Headline = styled.p`
 color:white;
@@ -61,6 +74,11 @@ font-weight: bold;
 line-height: 20px;
 margin-left: 16px;
 text-align: left;
+@media only screen and (max-width: 500px) {
+   font-weight: 18px;
+   margin-top:25px;
+   margin-bottom: 30px;
+    }
 `;
 export const Arrow = styled.div`
 flex:50%;
